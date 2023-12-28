2e1fde5d8bbd55d1d59194ecaa235ceb0bfbf499
ajwlforever <2353350597@qq.com>
2023-12-28 23:49:29 +0800

    0.0.4 postdao

--- dao/post.go
+++ dao/post.go
@@ -5,12 +5,30 @@ import (
 	"go-blog/models"
 )
 
+const (
+	_updateFour = "update post  set title = ?, content = ? , intro = ?, flags = ? where id = ?"
+	_selectAll  = "select * from post"
+	_selectById = "select * from post where id = ?"
+	_insert     = "insert into post(id,title,content,intro,flags,creator_id,level)" +
+		" values (?,?,?,?,?,?,?)"
+	_updateDeleted = "update post set is_deleted = ? where id = ?"
+	_deleteFull    = "delete from post where id = ?"
+	_countAll      = "select count(*) from post"
+)
+
 type PostDao struct {
 }
 
+func (postDao *PostDao) GetAllPostCount() (count int64) {
+	err := db.Get(&count, _countAll)
+	if err != nil {
+		fmt.Printf("getAllPostCount failed:%v", err)
+	}
+	return
+}
 func (postDao *PostDao) SelectAllPost() (posts []models.Post, err error) {
-	sqlStr := "select * from post"
-	err = db.Select(&posts, sqlStr)
+	posts = []models.Post{}
+	err = db.Select(&posts, _selectAll)
 	if err != nil {
 		fmt.Printf("get all failed, err:%v\n", err)
 		return
@@ -19,9 +37,9 @@ func (postDao *PostDao) SelectAllPost() (posts []models.Post, err error) {
 	return
 }
 
-func (postDao *PostDao) SelectById(id int) (post models.Post) {
-	sqlStr := "select * from post where id = ?"
-	err := db.Get(&post, sqlStr, 1)
+func (postDao *PostDao) SelectPostById(id int) (post models.Post) {
+
+	err := db.Get(&post, _selectById, id)
 	if err != nil {
 		fmt.Printf("get failed, err:%v\n", err)
 		return
@@ -30,19 +48,52 @@ func (postDao *PostDao) SelectById(id int) (post models.Post) {
 	return
 }
 
-func (postDao *PostDao) SavePost(post models.Post) (res bool) {
-	sqlStr := "insert into post(id,title,content,intro,flags,creator_id,level)" +
-		" values (?,?,?,?,?,?,?)"
-	result, err := db.Exec(sqlStr, post.Id, post.Title, post.Content, post.Intro, post.Flags, post.CreatorId, post.Id)
+func (postDao *PostDao) SavePost(post *models.Post) (res bool) {
+
+	result, err := db.Exec(_insert, post.Id, post.Title, post.Content, post.Intro, post.Flags, post.CreatorId, post.Id)
 	if err != nil {
 		fmt.Printf("save failed, err:%v\n", err)
 		return false
 	}
-	insertID, err := result.LastInsertId()
+	affectRows, err := result.RowsAffected()
 	if err != nil {
 		fmt.Printf("get insert id failed, err:%v\n", err)
 		return
 	}
-	fmt.Printf("save Id:%v \n", insertID)
-	return true
+	fmt.Printf("affectRows:%v \n", affectRows)
+	return
+}
+func (postDao *PostDao) UpdatePostById(post *models.Post) (affectedRow int64) {
+	result, err := db.Exec(_updateFour, post.Title, post.Content, post.Intro, post.Flags, post.Id)
+	if err != nil {
+		fmt.Printf("UpdatePostById failed:%v", err)
+	}
+	affectedRow, err = result.RowsAffected()
+	if err != nil {
+		fmt.Printf("UpdatePostById failed, err:%v")
+	}
+	return
+}
+func (postDao *PostDao) DeleteById(id int) (affectedRow int64) {
+	result, err := db.Exec(_updateDeleted, 1, id) // 将is_deleted置1
+	if err != nil {
+		fmt.Printf("DeleteById failed%v", err)
+	}
+	affectedRow, err = result.RowsAffected()
+	if err != nil {
+		fmt.Printf("DeleteById failed, err:%v")
+	}
+	return
+
+}
+func (postDao *PostDao) FullDeleteById(id int) (affectedRow int64) {
+	result, err := db.Exec(_deleteFull, id)
+	if err != nil {
+		fmt.Printf("DeleteById failed%v", err)
+	}
+	affectedRow, err = result.RowsAffected()
+	if err != nil {
+		fmt.Printf("DeleteById failed, err:%v")
+	}
+	return
 }
--- post_test.go
+++ post_test.go
@@ -5,13 +5,14 @@ import (
 	"go-blog/dao"
 	"go-blog/models"
 	"testing"
+	"time"
 )
 
 func TestPostSelect(t *testing.T) {
 	fmt.Println("sss")
 	//initMysql()
 	postDao := new(dao.PostDao)
-	post := postDao.SelectById(1)
+	post := postDao.SelectPostById(1)
 	fmt.Println(post.Title)
 }
 
@@ -19,14 +20,15 @@ func TestSavePost(t *testing.T) {
 	postDao := new(dao.PostDao)
 	var post models.Post
 	post.Title = "123"
-	post.Id = 12
+	post.Id = 1233
 	post.Content = "CCCCCC"
 	post.Intro = "1111"
 	post.Flags = "falgs"
 	post.CreatorId = 1
 	post.Level = 1
-
-	res := postDao.SavePost(post)
+	post.CreatedTime = time.Now()
+	post.ModifiedTime = time.Now()
+	res := postDao.SavePost(&post)
 
 	fmt.Printf("res: %v", res)
 }
@@ -38,5 +40,34 @@ func TestSelectAllPost(t *testing.T) {
 	if err != nil {
 		fmt.Println("err")
 	}
+	fmt.Println(len(posts))
 	fmt.Println(posts)
+}
+
+func TestAll1(t *testing.T) {
+	postDao := new(dao.PostDao)
+	fmt.Println(postDao.GetAllPostCount())
+	postDao.FullDeleteById(123)
+	fmt.Println(postDao.GetAllPostCount())
+	var post models.Post
+	post.Title = "123"
+	post.Id = 123
+	post.Content = "CCCCCC"
+	post.Intro = "1111"
+	post.Flags = "falgs"
+	post.CreatorId = 1
+	post.CreatedTime = time.Now()
+	post.ModifiedTime = time.Now()
+	post.Level = 1
+	postDao.SavePost(&post)
+	fmt.Println(postDao.GetAllPostCount())
+	postDao.DeleteById(123)
+	fmt.Printf("is_deleted:%v", postDao.SelectPostById(123))
+	post.Title = "llll"
+	post.Id = 12
+	post.Content = "cccc"
+	post.Intro = "aaaa"
+	post.Flags = "bbbbb"
+	postDao.UpdatePostById(&post)
+
 }
